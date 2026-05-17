c7353e0b8b33d2880afbc2c3cd733135043e49f9
quantixtechnology <mukhtar@quantixtechnology.in>
2026-05-17 12:31:43 +0530

    chore: auto-deploy 2026-05-17 12:31

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import ScrollRestoration from "@/app/components/ui/ScrollRestoration";
 
